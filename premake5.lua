36930d6ca977593a3d2c17c77dfb8269a06cf77b
tomosh22 <tomosh22@googlemail.com>
2023-10-29 23:23:53 +0000

    tesselation/displacement mapping

--- premake5.lua
+++ premake5.lua
@@ -56,17 +56,13 @@ project "GLFW"
 
 	filter "configurations:Debug"
 		runtime "Debug"
-		buildoptions "/MT"
 		symbols "on"
 
 	filter "configurations:Release"
 		runtime "Release"
-		buildoptions "/MT"
 		optimize "on"
 
 	filter "configurations:Dist"
 		runtime "Release"
-		buildoptions "/MT"
 		optimize "on"
-        symbols "off"
 		
